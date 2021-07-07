50bb7cddff6a3f6a480ad5acc7fd5ea97a38ee21
hxie0 <xhy@chicagobooth.edu>
2021-07-07 05:25:02 +0000

    math completed!

--- date_formatted.rb
+++ date_formatted.rb
@@ -1,3 +1,6 @@
 # Should format and identify different parts of todays date.
 # 
 # "The year is: 2020, the calendar day is: 1, and the month is: 7."
+require "date"
+dat = Date.today
+p "The year is: "+dat.year.to_s+", the calendar day is: "+dat.day.to_s+", and the month is: "+dat.month.to_s+"."
